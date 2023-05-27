082c72450cea4e9186b4b644b7d17e199873a543
NikitaPalmDev <nikitafedorov7@yandex.ru>
2023-05-27 17:25:44 +0300

    fix

--- src/lib/entities/Easter/ui/Peca.svelte
+++ src/lib/entities/Easter/ui/Peca.svelte
@@ -30,7 +30,7 @@
     })
 
     $: hours = $easterContext?.date.getHours()
-    $: show = hours ? (hours >= 16 || hours <= 1) : false
+    $: show = hours ? (hours >= 22 || hours <= 1) : false
     
 </script>
 {#if show}
