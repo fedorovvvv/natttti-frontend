0281e5e1712a9ee078f99beec8b846203afe8983
NikitaPalmDev <nikitafedorov7@yandex.ru>
2023-11-22 00:23:54 +0400

    fix

--- src/routes/(global)/(app)/(open)/account/settings/socials/+page.svelte
+++ src/routes/(global)/(app)/(open)/account/settings/socials/+page.svelte
@@ -1,19 +1,9 @@
 <script lang='ts'>
-	import { userStore } from "$appLayer/stores/user";
-	import { UsersQueries } from "$entities/users/index.js";
 	import { UsersSocialsUpdate } from "$features/users/socials/ui";
-	import type { SocialsResponse } from "$shared/api/pocketbase";
-	import { createQuery } from "@tanstack/svelte-query";
 
 	export let data
 
-	const socialsQuery = createQuery({
-		queryKey: UsersQueries.getSocials.createKey($userStore.current!.id),
-		queryFn: async (data) => UsersQueries.getSocials.queryFn(data),
-		initialData: data.socials as SocialsResponse
-	})
-
 </script>
-{#if data.user && $socialsQuery.isSuccess && $socialsQuery.data}
-	<UsersSocialsUpdate userId={data.user.id} initialData={$socialsQuery.data}/>
+{#if data.socials && data.user}
+	<UsersSocialsUpdate userId={data.user.id} initialData={data.socials}/>
 {/if}
