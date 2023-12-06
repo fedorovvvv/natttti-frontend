8ab6faabdd02220cfb27082b3006fd995349495d
Nikita Fedorov <50797337+fedorovvvv@users.noreply.github.com>
2023-12-06 21:39:38 +0400

    Revert "<3"

--- src/routes/(global)/(app)/(open)/events/gift/[id]/+page.svelte
+++ src/routes/(global)/(app)/(open)/events/gift/[id]/+page.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-
+	import { GiftEventsFeature } from '$features/giftEvents'
 	import { SectionContainer, SectionTitle } from '$shared/ui/Section'
+
+	export let data
 </script>
 
 <SectionContainer>
-	<SectionTitle>
-		<h2>Увы, отдел Тайного Санты закрылся</h2>
-		<p>Если вы очень хотите принять в нем участие, то вы можете организовать своего Тайного Санту через любые другие сервисы</p>
-		<br>
-		<small>Всем спасибо.</small>
-	</SectionTitle>
+	<GiftEventsFeature.Card giftEventId={data.id} initialData={data.giftEvent} />
 </SectionContainer>
